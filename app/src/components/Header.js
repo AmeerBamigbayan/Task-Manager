06725d27e842d274e82b18e731ac8da73ddb54cc
Ameer_105 <ameerdamilola@gmail.com>
2022-07-13 04:15:37 +0100

    .

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="header">
         <h1>Task Tracker</h1> 
         <button className="btn">Add Task</button>
-        <div className="btn2">SECOND BUTTON</div>
+        
     </header>
 
    
