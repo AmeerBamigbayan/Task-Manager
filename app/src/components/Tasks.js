b03b8bdd55b87b97f1e675ef4d06775425d40f30
Ameer_105 <ameerdamilola@gmail.com>
2022-07-13 22:35:25 +0100

    Update Tasks.js

---
+++ app/src/components/Tasks.js
@@ -0,0 +1,19 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Task 1',
+        completed: false
+    }
+
+
+]
+
+const Tasks = () => {
+  return (
+    <>
+    Tasks
+    </>
+  )
+}
+
+export default Tasks
